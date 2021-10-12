78dce2df15f6c81d14c7383f68182bafbbf7d3eb
Sebastian Ocampo <sebastianocampolopez507@gmail.com>
2021-10-12 17:07:56 -0500

    backend upgrade

--- Back/mercaTodo_API/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductsView
-
-urlpatterns = [
-    path('products/', ProductsView.as_view(), name='products_list'),
-    path('products/<int:prod_id>', ProductsView.as_view(), name='product'),
-]
